55318a29b7d4d9f9402b527a8764eec3cbb1516f
Anke <a.stein@posteo.de>
2021-10-05 17:18:46 +0200

    Replace fetch by axios

--- frontend/src/service/todo-api-service.js
+++ frontend/src/service/todo-api-service.js
@@ -1,50 +1,32 @@
+import axios from "axios";
+
 export const getTodos = () => {
-    return fetch("/api/todo")
-        .then(response => {
-                if (response.ok) {
-                    return response.json();
-                }
-                throw new Error("fetch todos failed")
-            }
-        )
+    return axios
+        .get('/api/todo')
+        .then(response => response.data)
 }
 
-export const postTodo = (description) => {
-    return fetch('/api/todo', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({description, status: "OPEN"}),
-    }).then(response => {
-            if (response.ok) {
-                return response.json();
-            }
-            throw new Error("fetch todos failed")
-        }
-    )
+export const postTodo = (newDescription) => {
+    const newTodo = {
+        description: newDescription,
+        status: 'OPEN'
+    };
+    return axios
+        .post('/api/todo', newTodo)
+        .then(response => response.data)
 }
+
 
 export const putTodo = (todo) => {
-    return fetch(`/api/todo/${todo.id}`, {
-        method: 'PUT',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(todo),
-    }).then(response => {
-            if (response.ok) {
-                return response.json();
-            }
-            throw new Error("update todo failed")
-        }
-    )
+    return axios
+        .put(`/api/todo/${todo.id}`, todo)
+        .then(response => response.data)
 }
 
+
 export const deleteTodo = (id) => {
-    return fetch(`/api/todo/${id}`, {
-        method: 'DELETE',
-    })
+    return axios
+        .delete(`/api/todo/${id}`)
 }
 
 
